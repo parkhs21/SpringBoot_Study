c8bc867618a1729abc6015cdc4c7227552c154b7
헥스/박해승 <2001phs92@gmail.com>
2023-11-20 20:54:54 +0900

    change db mode & main class rename

--- src/main/java/umc/study/SpringBootStudyApplication.java
+++ src/main/java/umc/study/SpringBootStudyApplication.java
@@ -6,10 +6,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class StudyApplication {
+public class SpringBootStudyApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StudyApplication.class, args);
+        SpringApplication.run(SpringBootStudyApplication.class, args);
     }
 
 }
